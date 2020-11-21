ddc684f75a2cb37c21984e9a7440f33dd15ae916
miguel <tsambamiguelito@gmail.com>
2020-11-21 14:06:05 +0100

    feat: Init homepage for teaser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { timer } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kaiflix';
+  myStyle: any;
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
+  ngOnInit(): void {
+    this.myStyle = {
+      position: 'fixed',
+      width: '100%',
+      height: '50%',
+      'z-index': -3,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    };
+
+    this.myParams = {
+      particles: {
+        number: {
+          value: 90,
+        },
+        color: {
+          value: '#ff0000',
+        },
+        shape: {
+          type: 'triangle',
+        },
+      },
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountdownTimeComponent } from './countdown-time/countdown-time.component';
+import { ParticlesModule } from 'angular-particle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountdownTimeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
